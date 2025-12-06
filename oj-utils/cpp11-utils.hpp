ec1831a50d7ede21c53e495bdf40b81287cc101f
liginity <liginity@outlook.com>
2025-12-06 19:46:46 +0800

    Add oj utils
    Add utils to print vector<int> and vector<vector<int>>.

---
+++ oj-utils/cpp11-utils.hpp
@@ -0,0 +1,32 @@
+#ifndef CPP11_UTILS_HPP_
+#define CPP11_UTILS_HPP_
+
+#include <iostream>
+#include <vector>
+
+// macros for debug
+#define SHOW(...)                                               \
+    {                                                           \
+        std::cout << #__VA_ARGS__ " = " << __VA_ARGS__ << "\n"; \
+    }
+
+inline std::ostream &operator<<(std::ostream &os, const std::vector<int> &vec) {
+    os << "{";
+    for (const auto &elem : vec) {
+        os << elem << ", ";
+    }
+    os << "}";
+    return os;
+}
+
+inline std::ostream &operator<<(std::ostream &os,
+                                const std::vector<std::vector<int>> &vec) {
+    os << "{\n";
+    for (const auto &elem : vec) {
+        os << "    " << elem << ",\n";
+    }
+    os << "}";
+    return os;
+}
+
+#endif
