c801c65cb341a7fe09f03f770f50ec4a5ec9dafa
liginity <liginity@outlook.com>
2026-01-12 20:06:55 +0800

    WIP remove SHOWLN() and others

--- include/learn/ojutils.hpp
+++ include/learn/ojutils.hpp
@@ -6,43 +6,26 @@
 // macros for debug
 #if __cplusplus < 202302L
 #include <iostream>
-#define SHOWLN(...)                                               \
+#define SHOW(...)                                                 \
     {                                                             \
         std::cout << #__VA_ARGS__ " = " << (__VA_ARGS__) << "\n"; \
-    }
-#define SHOW(...)                                         \
-    {                                                     \
-        std::cout << #__VA_ARGS__ " = " << (__VA_ARGS__); \
     }
 
 #else
 // c++23
 #include <print>
 
-#define SHOWLN(...)                                        \
+#define SHOW(...)                                          \
     {                                                      \
         std::print(#__VA_ARGS__ " = {}\n", (__VA_ARGS__)); \
     }
 
-#define SHOW(...)                                        \
-    {                                                    \
-        std::print(#__VA_ARGS__ " = {}", (__VA_ARGS__)); \
-    }
-
 inline const char INDENT_SPACES[16 + 1] = {"                "};
-
-#define SHOWLN_INDENT(indent_level, ...)                              \
-    {                                                                 \
-        assert(indent_level <= 4);                                    \
-        std::print("{}" #__VA_ARGS__ " = {}\n",                       \
-                   std::string_view(INDENT_SPACES, 4 * indent_level), \
-                   (__VA_ARGS__));                                    \
-    }
 
 #define SHOW_INDENT(indent_level, ...)                                \
     {                                                                 \
         assert(indent_level <= 4);                                    \
-        std::print("{}" #__VA_ARGS__ " = {}",                         \
+        std::print("{}" #__VA_ARGS__ " = {}\n",                       \
                    std::string_view(INDENT_SPACES, 4 * indent_level), \
                    (__VA_ARGS__));                                    \
     }
