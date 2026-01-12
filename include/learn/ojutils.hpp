1a22228e464dc0ab57854f7e64a724a703f95123
liginity <liginity@outlook.com>
2026-01-12 20:05:14 +0800

    WIP add SHOWLN() and SHOW_INDENT()
    I suddenly feel that, making SHOW() not print a new line does not make it work well.
    It could not be used multiple times and print good content.
    Maybe just keep the old SHOW(), and add SHOW_INDENT().

--- include/learn/ojutils.hpp
+++ include/learn/ojutils.hpp
@@ -1,24 +1,58 @@
 #ifndef LEARN_OJUTILS_HPP_
 #define LEARN_OJUTILS_HPP_
 
-#include <iostream>
-#include <vector>
+#include <cassert>
 
 // macros for debug
 #if __cplusplus < 202302L
-#define SHOW(...)                                                 \
+#include <iostream>
+#define SHOWLN(...)                                               \
     {                                                             \
         std::cout << #__VA_ARGS__ " = " << (__VA_ARGS__) << "\n"; \
     }
+#define SHOW(...)                                         \
+    {                                                     \
+        std::cout << #__VA_ARGS__ " = " << (__VA_ARGS__); \
+    }
+
 #else
+// c++23
 #include <print>
-#define SHOW(...)                                          \
+
+#define SHOWLN(...)                                        \
     {                                                      \
         std::print(#__VA_ARGS__ " = {}\n", (__VA_ARGS__)); \
     }
+
+#define SHOW(...)                                        \
+    {                                                    \
+        std::print(#__VA_ARGS__ " = {}", (__VA_ARGS__)); \
+    }
+
+inline const char INDENT_SPACES[16 + 1] = {"                "};
+
+#define SHOWLN_INDENT(indent_level, ...)                              \
+    {                                                                 \
+        assert(indent_level <= 4);                                    \
+        std::print("{}" #__VA_ARGS__ " = {}\n",                       \
+                   std::string_view(INDENT_SPACES, 4 * indent_level), \
+                   (__VA_ARGS__));                                    \
+    }
+
+#define SHOW_INDENT(indent_level, ...)                                \
+    {                                                                 \
+        assert(indent_level <= 4);                                    \
+        std::print("{}" #__VA_ARGS__ " = {}",                         \
+                   std::string_view(INDENT_SPACES, 4 * indent_level), \
+                   (__VA_ARGS__));                                    \
+    }
+
 #endif
 
+
 #if __cplusplus < 202302L
+#include <vector>
+
 inline std::ostream &operator<<(std::ostream &os, const std::vector<int> &vec) {
     os << "{";
     for (const auto &elem : vec) {
